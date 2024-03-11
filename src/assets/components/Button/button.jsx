b9a6c8b713c9be3fe19aa5820c9e2ee6a06e317d
Quocan1410 <110295693+Quocan1410@users.noreply.github.com>
2024-03-11 22:33:00 +1100

    feat: add Tailwind and add UI for function button

--- src/assets/components/Button/button.jsx
+++ src/assets/components/Button/button.jsx
@@ -9,62 +9,89 @@ function Button() {
         <div className="btnDiff__i2">Medium</div>
         <div className="btnDiff__i2">Hard</div>
       </div>
-      <div className="btnFunc__wrap">
+      <div className="btnFunc__wrap mt-4 flex w-[400px] cursor-pointer select-none items-center justify-around gap-6">
         {/* Btn item 1  */}
-        <button className="btn__item">
-          <div className="btn__icon btnFunc__img btnFunc__reverse">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 512 512"
-              fill="currentColor"
-            >
-              <path d="M75 75L41 41C25.9 25.9 0 36.6 0 57.9V168c0 13.3 10.7 24 24 24H134.1c21.4 0 32.1-25.9 17-41l-30.8-30.8C155 85.5 203 64 256 64c106 0 192 86 192 192s-86 192-192 192c-40.8 0-78.6-12.7-109.7-34.4c-14.5-10.1-34.4-6.6-44.6 7.9s-6.6 34.4 7.9 44.6C151.2 495 201.7 512 256 512c141.4 0 256-114.6 256-256S397.4 0 256 0C185.3 0 121.3 28.7 75 75zm181 53c-13.3 0-24 10.7-24 24V256c0 6.4 2.5 12.5 7 17l72 72c9.4 9.4 24.6 9.4 33.9 0s9.4-24.6 0-33.9l-65-65V152c0-13.3-10.7-24-24-24z" />
-            </svg>
+        <div className="btn__item group flex items-center gap-1 rounded-full bg-[#DBDBDA] py-[10px] pl-[10px] pr-[6px] hover:bg-[#d4d4d8]">
+          <button className="btn__icon btnFunc__img btnFunc__reverse w-fit rounded-full">
+            <div className="h-7 w-7 text-[#7F7F7F]">
+              <svg
+                className="icon"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 512 512"
+                fill="currentColor"
+                stroke="currentColor"
+              >
+                <path d="M75 75L41 41C25.9 25.9 0 36.6 0 57.9V168c0 13.3 10.7 24 24 24H134.1c21.4 0 32.1-25.9 17-41l-30.8-30.8C155 85.5 203 64 256 64c106 0 192 86 192 192s-86 192-192 192c-40.8 0-78.6-12.7-109.7-34.4c-14.5-10.1-34.4-6.6-44.6 7.9s-6.6 34.4 7.9 44.6C151.2 495 201.7 512 256 512c141.4 0 256-114.6 256-256S397.4 0 256 0C185.3 0 121.3 28.7 75 75zm181 53c-13.3 0-24 10.7-24 24V256c0 6.4 2.5 12.5 7 17l72 72c9.4 9.4 24.6 9.4 33.9 0s9.4-24.6 0-33.9l-65-65V152c0-13.3-10.7-24-24-24z" />
+              </svg>
+            </div>
+          </button>
+          <div className="duration-[250ms] w-0 text-[#7F7F7F] opacity-0 transition-all group-hover:ml-1 group-hover:w-16 group-hover:opacity-100">
+            Reverse
           </div>
-        </button>
+        </div>
 
         {/* Btn item 2  */}
-        <button className="btn__item">
-          <div className="btn__icon btnFunc__img btnFunc__hint">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="-65 0 512 512"
-              fill="currentColor"
-            >
-              <path d="M272 384c9.6-31.9 29.5-59.1 49.2-86.2l0 0c5.2-7.1 10.4-14.2 15.4-21.4c19.8-28.5 31.4-63 31.4-100.3C368 78.8 289.2 0 192 0S16 78.8 16 176c0 37.3 11.6 71.9 31.4 100.3c5 7.2 10.2 14.3 15.4 21.4l0 0c19.8 27.1 39.7 54.4 49.2 86.2H272zM192 512c44.2 0 80-35.8 80-80V416H112v16c0 44.2 35.8 80 80 80zM112 176c0 8.8-7.2 16-16 16s-16-7.2-16-16c0-61.9 50.1-112 112-112c8.8 0 16 7.2 16 16s-7.2 16-16 16c-44.2 0-80 35.8-80 80z" />
-            </svg>
-            <span className="hint__badge">5</span>
+        <div className="btn__item group relative flex items-center gap-0 rounded-full bg-[#DBDBDA] p-[10px] hover:bg-[#d4d4d8]">
+          <button className="btn__icon btnFunc__img btnFunc__hint w-fit rounded-full">
+            <div className="h-7 w-7 text-[#7F7F7F]">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="-65 0 512 512"
+                fill="currentColor"
+              >
+                <path d="M272 384c9.6-31.9 29.5-59.1 49.2-86.2l0 0c5.2-7.1 10.4-14.2 15.4-21.4c19.8-28.5 31.4-63 31.4-100.3C368 78.8 289.2 0 192 0S16 78.8 16 176c0 37.3 11.6 71.9 31.4 100.3c5 7.2 10.2 14.3 15.4 21.4l0 0c19.8 27.1 39.7 54.4 49.2 86.2H272zM192 512c44.2 0 80-35.8 80-80V416H112v16c0 44.2 35.8 80 80 80zM112 176c0 8.8-7.2 16-16 16s-16-7.2-16-16c0-61.9 50.1-112 112-112c8.8 0 16 7.2 16 16s-7.2 16-16 16c-44.2 0-80 35.8-80 80z" />
+              </svg>
+            </div>
+          </button>
+          <div className="btn__text duration-[250ms] w-0 text-[#7F7F7F] opacity-0 transition-all group-hover:ml-1 group-hover:w-8 group-hover:opacity-100">
+            Hint
           </div>
-        </button>
+
+          <span className="hint__badge absolute -right-1 -top-1 flex h-4 w-4 items-center justify-center rounded-full bg-[#7F7F7F] align-middle text-[9px] text-white">
+            5
+          </span>
+        </div>
 
         {/* Btn item 3  */}
-        <button className="btn__item">
-          <div className="btn__icon btnFunc__img btnFunc__note">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 512 512"
-              fill="currentColor"
-            >
-              <path d="M410.3 231l11.3-11.3-33.9-33.9-62.1-62.1L291.7 89.8l-11.3 11.3-22.6 22.6L58.6 322.9c-10.4 10.4-18 23.3-22.2 37.4L1 480.7c-2.5 8.4-.2 17.5 6.1 23.7s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L387.7 253.7 410.3 231zM160 399.4l-9.1 22.7c-4 3.1-8.5 5.4-13.3 6.9L59.4 452l23-78.1c1.4-4.9 3.8-9.4 6.9-13.3l22.7-9.1v32c0 8.8 7.2 16 16 16h32zM362.7 18.7L348.3 33.2 325.7 55.8 314.3 67.1l33.9 33.9 62.1 62.1 33.9 33.9 11.3-11.3 22.6-22.6 14.5-14.5c25-25 25-65.5 0-90.5L453.3 18.7c-25-25-65.5-25-90.5 0zm-47.4 168l-144 144c-6.2 6.2-16.4 6.2-22.6 0s-6.2-16.4 0-22.6l144-144c6.2-6.2 16.4-6.2 22.6 0s6.2 16.4 0 22.6z" />
-            </svg>
-            <span className="note__badge">OFF</span>
+        <div className="btn__item btn__item3 group relative flex items-center gap-1 text-nowrap rounded-full bg-[#DBDBDA] py-[10px] pl-[10px] pr-[6px] hover:bg-[#d4d4d8]">
+          <button className="btn__icon btnFunc__img btnFunc__note w-fit rounded-full">
+            <div className="h-[24px] w-[24px] text-[#7F7F7F]">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 512 512"
+                fill="currentColor"
+              >
+                <path d="M410.3 231l11.3-11.3-33.9-33.9-62.1-62.1L291.7 89.8l-11.3 11.3-22.6 22.6L58.6 322.9c-10.4 10.4-18 23.3-22.2 37.4L1 480.7c-2.5 8.4-.2 17.5 6.1 23.7s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L387.7 253.7 410.3 231zM160 399.4l-9.1 22.7c-4 3.1-8.5 5.4-13.3 6.9L59.4 452l23-78.1c1.4-4.9 3.8-9.4 6.9-13.3l22.7-9.1v32c0 8.8 7.2 16 16 16h32zM362.7 18.7L348.3 33.2 325.7 55.8 314.3 67.1l33.9 33.9 62.1 62.1 33.9 33.9 11.3-11.3 22.6-22.6 14.5-14.5c25-25 25-65.5 0-90.5L453.3 18.7c-25-25-65.5-25-90.5 0zm-47.4 168l-144 144c-6.2 6.2-16.4 6.2-22.6 0s-6.2-16.4 0-22.6l144-144c6.2-6.2 16.4-6.2 22.6 0s6.2 16.4 0 22.6z" />
+              </svg>
+            </div>
+          </button>
+          <div className="duration-[250ms] w-0 text-[#7F7F7F] opacity-0 transition-all group-hover:ml-1 group-hover:w-10 group-hover:opacity-100">
+            Note
           </div>
-        </button>
+
+          <div className="note__badge absolute -right-1 -top-1 flex h-4 w-4 items-center justify-center rounded-full bg-[#7F7F7F] text-[7.5px] text-white">
+            ON
+          </div>
+        </div>
 
         {/* Btn item 4  */}
-        <button className="btn__item">
-          <div className="btn__icon btnFunc__img">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 640 512"
-              fill="currentColor"
-            >
-              <path d="M192 64C86 64 0 150 0 256S86 448 192 448H448c106 0 192-86 192-192s-86-192-192-192H192zM496 168a40 40 0 1 1 0 80 40 40 0 1 1 0-80zM392 304a40 40 0 1 1 80 0 40 40 0 1 1 -80 0zM168 200c0-13.3 10.7-24 24-24s24 10.7 24 24v32h32c13.3 0 24 10.7 24 24s-10.7 24-24 24H216v32c0 13.3-10.7 24-24 24s-24-10.7-24-24V280H136c-13.3 0-24-10.7-24-24s10.7-24 24-24h32V200z" />
-            </svg>
+        <div className="btn__item group flex items-center gap-1 truncate rounded-full bg-[#DBDBDA] py-[10px] pl-[10px] pr-[6px] hover:bg-[#d4d4d8]">
+          <button className="btn__icon btnFunc__img w-fit rounded-full">
+            <div className="flex h-7 w-7 items-center justify-center text-[#7F7F7F]">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 640 512"
+                fill="currentColor"
+              >
+                <path d="M192 64C86 64 0 150 0 256S86 448 192 448H448c106 0 192-86 192-192s-86-192-192-192H192zM496 168a40 40 0 1 1 0 80 40 40 0 1 1 0-80zM392 304a40 40 0 1 1 80 0 40 40 0 1 1 -80 0zM168 200c0-13.3 10.7-24 24-24s24 10.7 24 24v32h32c13.3 0 24 10.7 24 24s-10.7 24-24 24H216v32c0 13.3-10.7 24-24 24s-24-10.7-24-24V280H136c-13.3 0-24-10.7-24-24s10.7-24 24-24h32V200z" />
+              </svg>
+            </div>
+          </button>
+          <div className="duration-[250ms] w-0 text-[#7F7F7F] opacity-0 transition-all group-hover:ml-1 group-hover:w-[78px] group-hover:opacity-100">
+            New game
           </div>
-        </button>
+        </div>
       </div>
-      <div className="overlay"></div>
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import Header from './assets/components/Header/header'
-import Button from './assets/components/Button/button'
-import Table from './assets/components/Table/table'
-import Numbers from './assets/components/Numbers/number'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Header from './assets/components/Header/header';
+import Button from './assets/components/Button/button';
+import Table from './assets/components/Table/table';
+import Numbers from './assets/components/Numbers/number';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
-    <Header></Header>
-    <Button></Button>
-    <Table></Table>
-    <Numbers></Numbers>
-  </>,
-)
-
+    <Header />
+    <Button />
+    <Table />
+    <Numbers />
+  </>
+);
