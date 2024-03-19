4fd168a64770de822677805f085091ebde49cec7
phuchauxd12 <118866081+phuchauxd12@users.noreply.github.com>
2024-03-19 22:45:02 +0700

    fix: change note color, change number of return and hint, add press number

--- src/assets/components/Numbers/number.jsx
+++ src/assets/components/Numbers/number.jsx
@@ -1,18 +1,38 @@
 import "./number.css"
 import Table from "../Table/table";
 import Button from "../Button/button";
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 function Numbers(){
     const[number,setNumber] = useState();
     const [note,setNote] = useState("OFF");
     const [data, setData] = useState();
     const [hint, setHint] = useState(5);
+    const [returnBack, setReturnBack] = useState(5);
     const[level,setLevel] = useState('easy');
     const [triggerFetch, setTriggerFetch] = useState(true);
     const [arrNote, setArrayNote] = useState(() => {
       return { arr: generateInitialArray() };
     });
+    useEffect(() => {
+      const handleKeyDown = (event) => {
+          const keyCode = event.keyCode;
+          if (keyCode >= 49 && keyCode <= 57) {
+              const number = keyCode - 48; // Convert keyCode to actual number
+              if(note =="OFF"){
+                handlePress(number);
+                }
+                else {
+                  handleNote(number);
+                }
+          }
+      };
 
+      document.addEventListener('keydown', handleKeyDown);
+
+      return () => {
+          document.removeEventListener('keydown', handleKeyDown);
+      };
+  }, [note]);
     const actionList = useRef([]);
     function generateInitialArray() {
       const arr = [];
@@ -33,7 +53,7 @@ function Numbers(){
       setNote(state=> state==="OFF"?"ON":"OFF")
     };
     
-    function handlePress(e) {
+    function handlePress(numberChoose) {
       const fillBox = document.querySelector(".isSelect");
       // Fill the number in the emty box
       if (fillBox && fillBox.querySelector(".number").textContent ==="") {
@@ -45,7 +65,7 @@ function Numbers(){
       const numberColum = fillBox.classList[1][4];
       const numberRow = fillBox.parentNode.id[4]
       const action = {
-        value: e.target.textContent,
+        value: numberChoose,
         row: numberRow,
         colum: numberColum,
       };
@@ -55,6 +75,7 @@ function Numbers(){
     }
 
     function handleReturn(){
+      if(returnBack>0 && actionList.current!="") {
       const prevAction = actionList.current.pop();
       
       if(prevAction && prevAction.value){
@@ -64,9 +85,11 @@ function Numbers(){
           value:"",
         })
       }
+      setReturnBack(prev=>prev-1);
+      }
     };
     
-    function handleNote(e){
+    function handleNote(numberChoose){
          const fillBox = document.querySelector(".isSelect");
       // Fill the number in the emty box
       if (fillBox && fillBox.querySelector(".number").textContent ==="") {
@@ -78,8 +101,8 @@ function Numbers(){
         const newArr = [...prevArr.arr];
     
         // Update the specific property at the given indices
-        const value =  newArr[numberRow][numberColum][e.target.textContent-1];
-        newArr[numberRow][numberColum][e.target.textContent-1] = e.target.textContent==value?"":e.target.textContent;
+        const value =  newArr[numberRow][numberColum][numberChoose-1];
+        newArr[numberRow][numberColum][numberChoose-1] = numberChoose==value?"":numberChoose;
         // Return the updated array
         return {arr:newArr,
           row: numberRow,
@@ -148,8 +171,23 @@ function Numbers(){
       });
       setLevel(level);
       setTriggerFetch(prev => !prev);
-      setHint(5);
       setNote("OFF");
+      switch(level) {
+        case "easy":
+          setHint(5);
+          setReturnBack(5);
+          break;
+        case "medium":
+          setHint(5);
+          setReturnBack(3);
+          break;
+          case "hard":
+          setHint(3);
+          setReturnBack(3);
+          break;
+        default:
+          console.log("error");
+      }
       setArrayNote(() => { 
         const listNote = document.querySelectorAll(".noteNumber");
         listNote.forEach(element=>{
@@ -167,10 +205,10 @@ function Numbers(){
         <button key={i} 
         onClick={e=>{
           if(note =="OFF"){
-          handlePress(e);
+          handlePress(e.target.textContent);
           }
           else {
-            handleNote(e);
+            handleNote(e.target.textContent);
           }
         }}
         >{i}</button>
@@ -180,12 +218,13 @@ function Numbers(){
     return (
       <>
       <Button returnButton ={handleReturn} pressNoteButton ={pressNoteButton} note ={note} hint={hint} handleHint= {handleHint}
-        newGame= {newGame}></Button>
+        newGame= {newGame} returnBack= {returnBack}></Button>
       <Table pressNumber={number} note ={note} arrNote= {arrNote} actionList ={actionList} sendBackParentdata = {handleReceivedData}
         levelChose= {level} triggerFetch = {triggerFetch}/>
       <div id="numberContainer">
         {buttons}
       </div>
+      <div className="copyright">© 2024 RMIT Neo Culture Tech Club. All rights reserved. <a href="https://github.com/RmitNeoculturetechclub/NCT_Sudoku">GitHub</a></div>
       </>
     );
   }
